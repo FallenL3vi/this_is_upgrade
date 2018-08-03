e98a9a1336e8c414ce28a9f8cd245e83cddd0428
Seba123512 <seba12351212@gmail.com>
2018-08-03 17:45:14 +0200

    Add files via upload

--- entity.hpp
+++ entity.hpp
@@ -1,13 +1,12 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class Entity : public sf::Drawable 
+class Entity 
 {
 	private:
 	public:
-	Entity(int i, int j, sf::Texture texture);
+	Entity(int i, int j, sf::Texture &texture);
 	sf::Sprite sprite;
 	void move();
 	
-	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
